353c5f5eec1e2404c7b5b8769ada60496c8b0db0
lonelytransistor <christopher.815.0@gmail.com>
2022-02-12 15:42:41 +0100

    Repo update to first functional version without dummies

--- athena/rebuildRepo.sh
+++ athena/rebuildRepo.sh
@@ -3,10 +3,13 @@ rm Packages
 mkdir -p tmp/
 for fname in *.ipk ; do
     (
+        set -e
         cd tmp/
-        tar zxf ../${fname}
-        tar zxf control.tar.gz
+        tar zxf ../${fname} ./control.tar.gz
+        tar zxf control.tar.gz ./control
         cat control >> ../Packages
+	rm control.tar.gz
+	rm control
     )
     md5=($(md5sum ${fname}))
     size=($(stat --format=%s ${fname}))
